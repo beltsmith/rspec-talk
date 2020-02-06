079cbc3527201460e2b35d96b3432730a424cb4b
Alex Girdler <alex.girdler@sonder.com>
2020-02-06 14:05:32 -0800

    Final talk

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -7,20 +7,69 @@ RSpec::Matchers.define :be_a_warning do
   end
 end
 
-describe "warning" do
-  subject(:warning) { "WARN -- Something is messed up." }
-  it { is_expected.to start_with("WARNING:") }
-  it { is_expected.to end_with "!" }
+# describe "warning" do
+#   subject(:warning) { "WARN -- Something is messed up." }
+#   it { is_expected.to start_with("WARNING:") }
+#   it { is_expected.to end_with "!" }
 
-  it "passes our validations", aggregate_failures: true do
-    expect(warning).to start_with("WARNING:")
-    expect(warning).to end_with("!")
-  end
+#   it "passes our validations", aggregate_failures: true do
+#     expect(warning).to start_with("WARNING:")
+#     expect(warning).to end_with("!")
+#   end
 
-  it { is_expected.to start_with("WARNING:").and end_with("!") }
-end
+#   it { is_expected.to start_with("WARNING:").and end_with("!") }
+# end
+
 
 describe Bike do
+  shared_examples :shift_gear do ||
+  end
+
+  RSpec::Matchers.define :shift_gear_of do |bike|
+    match do |actual|
+      @delta ||= 1
+      @direction ||= nil
+
+      @previous_gear = bike.gear
+
+      actual.call
+
+      @actual_delta = bike.gear - @previous_gear
+      @actual_delta == @delta
+    end
+
+    chain :by, :delta
+
+    description do
+      case @direction
+      when :up
+        "shift gear up #{@delta}"
+      when :down
+        "shift gear down #{- @delta}"
+      else
+        "shift gear by #{@delta}"
+      end
+    end
+
+    failure_message do
+      actual_direction = @actual_delta > 0 ? :up : :down
+
+      ["expected block to #{description}",
+       "but shifted #{actual_direction} #{@actual_delta.abs}"].join(", ")
+    end
+
+    chain :up do |delta|
+      @direction = :up
+      @delta = delta
+    end
+
+    chain :down do |delta|
+      @direction = :down
+      @delta = - delta
+    end
+
+    supports_block_expectations
+  end
 
   shared_context "in highest gear" do
     let(:bike) { described_class.new(gear: Bike::MAX_GEAR) }
@@ -47,21 +96,25 @@ describe Bike do
         expect { shift_up }.to change(bike, :gear).by(1)
       end
 
+      specify { expect { shift_up }.to shift_gear_of(bike).by(1) }
+      specify { expect { shift_up }.to shift_gear_of(bike).up(1) }
+      specify { expect { shift_up }.to shift_gear_of(bike).down(1) }
+
       specify { expect { shift_up }.to change(bike, :gear).by(1) }
 
-      context "as a block" do
-        subject { -> { bike.shift_up } }
+      # context "as a block" do
+      #   subject { -> { bike.shift_up } }
 
-        specify "called multiple times changes gear each time" do
-          is_expected.to change(bike, :gear).by(1)
-          is_expected.to change(bike, :gear).by(1)
-          is_expected.to change(bike, :gear).by(1)
-          is_expected.to change(bike, :gear).by(1)
-          # expect { shift_up }.to change(bike, :gear).by(1)
-          # expect { shift_up }.to change(bike, :gear).by(1)
-          # expect { shift_up }.to change(bike, :gear).by(1)
-        end
-      end
+      #   specify "called multiple times changes gear each time" do
+      #     is_expected.to change(bike, :gear).by(1)
+      #     is_expected.to change(bike, :gear).by(1)
+      #     is_expected.to change(bike, :gear).by(1)
+      #     is_expected.to change(bike, :gear).by(1)
+      #     # expect { shift_up }.to change(bike, :gear).by(1)
+      #     # expect { shift_up }.to change(bike, :gear).by(1)
+      #     # expect { shift_up }.to change(bike, :gear).by(1)
+      #   end
+      # end
     end
   end
 
